26fbe6bc9d2efbf00f034692220649040a7e4dc8
Trallianz <bennet.seinnachname@gmail.com>
2025-01-16 22:22:29 +0100

    added firebase

--- vue-project/main2.js
+++ vue-project/main2.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue';
-import { VueFire } from "vuefire";
-import { initializeApp } from "firebase/app";
-import App from "./App.vue";
 import './style.css'
 import 'leaflet/dist/leaflet.css'
+import App from './App.vue';
+import { createVueFire, VueFireAuth } from 'vuefire';
+import { initializeApp } from 'firebase/app';
 
-export const firebaseApp = initializeApp({
+// Your web app's Firebase configuration
+const firebaseConfig = {
     apiKey: "AIzaSyCetbmiUpRv9f1bJUB0EdYj-kiJ9tytXVI",
     authDomain: "ontime-e0281.firebaseapp.com",
     databaseURL: "https://ontime-e0281-default-rtdb.europe-west1.firebasedatabase.app",
@@ -14,11 +15,16 @@ export const firebaseApp = initializeApp({
     messagingSenderId: "276717446742",
     appId: "1:276717446742:web:89786f17d2665a1f787df7",
     measurementId: "G-YWZEDZXH81"
-  });
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const vueFire = createVueFire({
+  firebaseApp,
+  modules: [VueFireAuth()],
+});
 
 const app = createApp(App);
 
-// install the VueFire plugin
-app.use(VueFire, { firebaseApp });
-
-app.mount("#app");
+app.use(vueFire);
+app.mount('#app');
