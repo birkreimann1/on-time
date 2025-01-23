5f066ea854804a3e6d302334760933016d8a0f28
Zorn <nicolai.zorn@gmx.de>
2025-01-23 15:05:40 +0100

    [update] Beschreibung des Scores

--- vue-project/src/components/ScoreExplanation.vue
+++ vue-project/src/components/ScoreExplanation.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        <p class="font-bold text-xl">Attribute</p>
-        <ul>
-          <li>Licht</li>
-          <li>Temperatur</li>
-          <li>Verkehr</li>
-          <li>Wetter</li>
-        </ul>
-        <p class="font-bold text-xl pt-4">Berechnung</p>
-        <p>a² + b² = c²</p>
-        </div>
+  <div>
+    <p class="font-bold text-xl pt-4">Beschreibung</p>
+      <ul>
+        <p>Dieser Score ergibt sich aus einer gewichteten Inzidenz, wobei geringere Verspätungen das Ergebnis weniger stark beeinflussen als längere.</p>
+        <p>Der Score wird gemittelt über die jeweiligen Werte aller derzeit erfassten Umweltfaktoren</p>
+
+      </ul>
+    <p class="font-bold text-xl pt-4">Berücksichtigte Umweltfaktoren</p>
+      <ul>
+        <li>Lichtverhältnisse</li>
+        <li>Temperatur</li>
+        <li>Verkehrsaufkommen</li>
+        <li>Wetter</li>
+      </ul>
+    <p class="font-bold text-xl pt-4">Berechnung</p>
+    <p>Score = 100 * (1 - (0,2 • V_kurz_n + 0,4 • V_mittel_n + 0,6 • V_lang_n + 0,8 • V_extrem_n + Ausfall_n)/Gesamtmessungen_n)</p>
+    </div>
 </template>
