cf92dc72128b778a7c000de6980a9f949aa855f7
Zorn <nicolai.zorn@gmx.de>
2025-01-27 20:20:31 +0100

    [fix] removed asterisk

--- vue-project/src/components/ScoreExplanation.vue
+++ vue-project/src/components/ScoreExplanation.vue
@@ -7,7 +7,7 @@
     <p class="font-bold text-xl pt-4 pb-2">Berechnung</p>
     <div class="bg-gray-100 p-4 rounded-lg shadow-md border border-gray-300">
       <p class="font-mono text-lg text-center text-black">
-        score = 100 * (1 - (0.5 • n<sub>kurz</sub> + 0.6 • n<sub>mittel</sub> + 0.7 • n<sub>lang</sub> + 0.8 • n<sub>extrem</sub> + n<sub>ausfall</sub>) / n<sub>gesamt</sub>)
+        score = 100 • (1 - (0.5 • n<sub>kurz</sub> + 0.6 • n<sub>mittel</sub> + 0.7 • n<sub>lang</sub> + 0.8 • n<sub>extrem</sub> + n<sub>ausfall</sub>) / n<sub>gesamt</sub>)
       </p>
     </div>
   </div>
